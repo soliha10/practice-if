4000be982cb377d059d555923bd254a05508cf3b
soliha10 <toxtamurodovagulshodaa@gmail.com>
2023-12-26 11:17:51 +0500

    First commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// var number = Math.abs(Number(prompt("Son kiriting"))) ;
+// if (number % 3 == 0 && number % 5 == 0) {
+//   console.log("FizzBuzz");
+// }
+
+// else if (number % 3 == 0){
+//   console.log("Fizz");
+// }
+// else if (number % 5 == 0){
+//   console.log("Buzz");
+// }
+// else {
+//   console.log("son 3 ga ham 5 ga ham bo'linmaydi");
+// }
+
+var randomNumber = Math.round(Math.random() * 100);
+console.log(randomNumber);
